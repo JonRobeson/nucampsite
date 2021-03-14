63caa2547e2913ed40a983f577e19e3b1ef82fec
Jon Robeson <jj.rrobeson@gmail.com>
2021-03-14 15:59:13 -0500

    week 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-$(function() {
-    $("#testBtn").click(function(){
-        $("#testModal").modal("show");
+// $(function () {
+//     $("#testBtn").click(function () {
+//         $("#testModal").modal("show");
+//     });
+// });
+
+
+$(function () {
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+});
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
     });
 });
